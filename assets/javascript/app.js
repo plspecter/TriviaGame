469b4c091203549192cfa0b69b41374270e85e84
Perri Specter <plspecter@gmail.com>
2018-08-25 21:30:04 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
-$()
+    $("#startButton").on("click", function () {
+        console.log("this registers");
+        var start = $("#startButton"); {
+         
+            if (start.style.display === "none") {
+            }
+
+        }
+
+
+
+
+
+    });
 
 });
