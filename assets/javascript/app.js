b0b19085179b203ba781a4f22c6e04baaa601af2
Perri Specter <plspecter@gmail.com>
2018-08-28 00:06:44 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,58 @@
-$(document).ready(function () {
+jQuery(document).ready(function () {
 
+    console.log("ready");
+
+    var stopwatch = 120;
+    var intervalId
+
+    var answers1 = "";
+
+    var correct = 0;
+    var incorrect = 1;
+    var timerRunning = false
+    var answers = false;
+    var timer = {
+        time: 0
+    }
+
+    //Can we target the answers to not appear until start button
+
+    //Hide the start button when i press it
     $("#startButton").on("click", function () {
         console.log("this registers");
-        var start = $("#startButton"); {
-         
-            if (start.style.display === "none") {
-            }
+        $("#startButton").text('Hide');
+        $("#startButton").toggle();
 
+
+        //My timer starts when start button is pressed
+        //stopwatch is decrementing down by the second (1000)
+        function run() {
+            if (!timerRunning)
+                intervalId = setInterval(stopwatch.count, 1000);
+            timerRunning = true;
+        }
+
+        function decrement() {
+            stopwatch--;
+            $("#timer").html("<h2>" + stopwatch + "</h2>")
+            console.log(stopwatch);
+
+            if (number === 0) {
+                stop();
+                alert("Time up!");
+            }
         }
 
 
 
 
 
-    });
+        //Hide the questions and answers until we press start
+        //Change the container size to fit all of the questions/answers when we press start
 
+        $(".answers1").text(answers1);
+        answers = true;
+
+    });
 });
+
